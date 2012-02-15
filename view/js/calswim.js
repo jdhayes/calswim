8e3e5b59bf0686b37aeab3f141dcd9fe1c0a0f8d
Jordan Hayes <hayesjordan@gmail.com>
2012-02-15 12:48:11 -0800

    Started implementing auto zoom and auto pan

--- view/js/calswim.js
+++ view/js/calswim.js
@@ -1,4 +1,29 @@
 // CalSWIM main Javascript file
+
+// Calculate nearest markers
+function rad(x) {return x*Math.PI/180;}
+function find_closest_marker( lat, lng, markers ) {
+    var R = 6371;
+    var distances = [];
+    var closest = -1;
+    for( i=0; i<markers.length; i++ ) {
+        var mlat = markers[i].position.lat();
+        var mlng = markers[i].position.lng();
+        var dLat  = rad(mlat - lat);
+        var dLong = rad(mlng - lng);
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(rad(lat)) * Math.cos(rad(lat)) * Math.sin(dLong/2) * Math.sin(dLong/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c;
+        distances[i] = d;
+        if ( closest == -1 || d < distances[closest] ) {
+            closest = i;
+        }
+    }
+
+    alert(markers[closest].title);
+}
+	
 $(document).ready(function() {	
 	
 	$('#map_canvas').gmap().bind('init', function() { 	
@@ -14,11 +39,14 @@ $(document).ready(function() {
 		});
 	});
 	
-	$('#search_button').click(function() {		
-		$('#map_canvas').gmap('search', { 'address': $("#search").val() }, function(results, status) {
-		    if ( status === 'OK' ) {
-		                $('#map_canvas').gmap('get', 'map').panTo(results[0].geometry.location);
-		        }
-		});
+	$('#search_button').click(function() {
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode( {'address': $('#search').val() }, function(results, status) { if (status == google.maps.GeocoderStatus.OK) {
+			var lat = results[0].geometry.location.lat();
+			var lng = results[0].geometry.location.lng();
+			var markers = $('#map_canvas').gmap('get', 'markers');
+			find_closest_marker( lat, lng, markers );
+			$('#map_canvas').gmap('get', 'map').panTo(results[0].geometry.location);
+		}				              	
 	});
 });
