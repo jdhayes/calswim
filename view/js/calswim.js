881e45064baeedf5fb144528cf24014d8083c3fc
Jordan Hayes <hayesjordan@gmail.com>
2012-02-15 10:14:05 -0800

    Fixed gmaps init function to execute when dom is ready

--- view/js/calswim.js
+++ view/js/calswim.js
@@ -1,13 +1,14 @@
 // CalSWIM main Javascript file
-
-$('#map_canvas').gmap().bind('init', function() { 	
-	$.getJSON( 'http://jquery-ui-map.googlecode.com/svn/trunk/demos/json/demo.json', function(data) { 
-		$.each( data.markers, function(i, marker) {
-			$('#map_canvas').gmap('addMarker', { 
-				'position': new google.maps.LatLng(marker.latitude, marker.longitude), 
-				'bounds': true 
-			}).click(function() {
-				$('#map_canvas').gmap('openInfoWindow', { 'content': marker.content }, this);
+$(document).ready(function() {
+	$('#map_canvas').gmap().bind('init', function() { 	
+		$.getJSON( 'http://jquery-ui-map.googlecode.com/svn/trunk/demos/json/demo.json', function(data) { 
+			$.each( data.markers, function(i, marker) {
+				$('#map_canvas').gmap('addMarker', { 
+					'position': new google.maps.LatLng(marker.latitude, marker.longitude), 
+					'bounds': true 
+				}).click(function() {
+					$('#map_canvas').gmap('openInfoWindow', { 'content': marker.content }, this);
+				});
 			});
 		});
 	});
