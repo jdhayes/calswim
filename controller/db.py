4882fbfe4b0deb04c47de72ae8784d5830d12e06
Jordan Hayes <hayesjordan@gmail.com>
2012-02-15 16:16:55 -0800

    Setup db.py to use DB instead of fake data

--- controller/db.py
+++ controller/db.py
@@ -11,46 +11,32 @@ def GetMapLocs():
         This is a simple script to query the database.
         We need this so that ajax can pull data
     """    
-#    select_query="SELECT KEGG,BIOCHEMICAL,HMDB_ID FROM LiverMetabolites;"      
-#
-#    # Connect to an existing database
-#    connParams = {}
-#    connParams["UID"] = "genomics"
-#    connParams["PWD"] = "genomics"
-#    connParams["HOST"] = "db-igb.ics.uci.edu"
-#    connParams["PORT"] = 5000
-#    connParams["DSN"] = "SassoneData"        
-#
-#    # Open database connection
-#    db = MySQLdb.connect(connParams["HOST"],connParams["UID"],connParams["PWD"],connParams["DSN"],connParams["PORT"])        
-#    # prepare a cursor object using cursor() method
-#    cursor = db.cursor()        
-#    # execute SQL query using execute() method.
-#    cursor.execute(select_query)        
-#    # Fetch a single row using fetchone() method.
-#    data = set()
-#    while(1):
-#        row=cursor.fetchone()
-#        if row == None:
-#            break
-#        for i in range(len(row)):
-#            data.add(str(row[i]))                
-#        
-#    # disconnect from server
-#    db.close()
-#
-#    # Return search values    
-#    data = list(data)
-#    data.sort()
-#    return json.dumps(data)
-    return """
-        {"markers":[ 
-            { "latitude":57.7973333, "longitude":12.0502107, "title":"Angered", "content":"Representing :)" },
-            { "latitude":57.6969943, "longitude":11.9865, "title":"Gothenburg", "content":"Swedens second largest city" },
-            
-            { "latitude":33.901303, "longitude":-117.520888, "title":"Rock Vista Park", "content":"2401 Moonridge Cir"},
-            { "latitude":33.90141, "longitude":-117.531102, "title":"Parkview Park", "content":"1995 Las Colinas Cir"},
-                        
-            { "latitude":33.64568, "longitude":-117.842533, "title":"Aldrich Park", "content":"University of California, Irvine, CA 92879"},
-            { "latitude":33.644371, "longitude":-117.841861, "title":"Information Computer Science Building", "content":"University of California, Irvine, CA 92879"}            
-        ]}"""
+    select_query="SELECT City,State,Latitude,Longitude FROM calswim__cities"      
+
+    # Connect to an existing database
+    connParams = {}
+    connParams["UID"] = "calswim"
+    connParams["PWD"] = "calswim2012"
+    connParams["HOST"] = "localhost"
+    connParams["PORT"] = 3306
+    connParams["DSN"] = "wikijoo"        
+
+    # Open database connection
+    db = MySQLdb.connect(connParams["HOST"],connParams["UID"],connParams["PWD"],connParams["DSN"],connParams["PORT"])        
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()        
+    # execute SQL query using execute() method.
+    cursor.execute(select_query)        
+    # Fetch a single row using fetchone() method.
+    data = set()
+    while(1):
+        row=cursor.fetchone()
+        if row == None:
+            break                
+        data.add( {"title":row[0]+", "+row[1], "content":row[0]+", "+row[1], "latitude":str(row[2]), "longitude":str(row[3])} )                
+        
+    # disconnect from server
+    db.close()
+
+    # Return search values as json
+    return json.dumps(data)
--- view/js/calswim.js
+++ view/js/calswim.js
@@ -1,38 +1,28 @@
 // CalSWIM main Javascript file
 
-// Calculate nearest markers
-function rad(x) {return x*Math.PI/180;}
-function find_closest_marker( lat, lng, markers ) {
-    alert(lat);
-	var R = 6371;
-    var distances = [];
-    var closest = -1;    
-    for( i=0; i<markers.length-1; i++ ) {
-        var mlat = markers[i].position.lat();
-        alert(mlat);
-        var mlng = markers[i].position.lng();
-        var dLat  = rad(mlat - lat);
-        var dLong = rad(mlng - lng);
-        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.cos(rad(lat)) * Math.cos(rad(lat)) * Math.sin(dLong/2) * Math.sin(dLong/2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        var d = R * c;
-        distances[i] = d;
-        if ( closest == -1 || d < distances[closest] ) {
-            closest = i;
-        }
-    }
+// Set Auto Zoom and Center
+// map: an instance of GMap2
+// latlng: an array of instances of GLatLng
+var glatlngs = new Arrary();
+var latlngbounds = new GLatLngBounds( );
 
-    alert(markers[closest].title);
+function auto_zoom_center() {
+	for ( var i = 0; i < glatlngs.length; i++ )
+	{
+	  latlngbounds.extend( glatlngs[ i ] );
+	}
+	map.setCenter( latlngbounds.getCenter( ), map.getBoundsZoomLevel( latlngbounds ) );
 }
-	
-$(document).ready(function() {
-	
+
+$(document).ready(function() {	
 	$('#map_canvas').gmap().bind('init', function() { 	
 		$.getJSON( '?get_map_locs=a', function(data) { 
-			$.each( data.markers, function(i, marker) {
+			$.each( data.markers, function(i, marker) {				
+				var glatlng = new google.maps.LatLng(marker.latitude, marker.longitude);
+				glatlngs.push(glatlng);
+				
 				$('#map_canvas').gmap('addMarker', { 
-					'position': new google.maps.LatLng(marker.latitude, marker.longitude), 
+					'position': glatlng), 
 					'bounds': true 
 				}).click(function() {
 					$('#map_canvas').gmap('openInfoWindow', { 'content': marker.content }, this);
@@ -43,7 +33,8 @@ $(document).ready(function() {
 	
 	$('#search_button').click(function() {
 		var geocoder = new google.maps.Geocoder();
-		geocoder.geocode( {'address': $('#search').val() }, function(results, status) { if (status == google.maps.GeocoderStatus.OK) {
+		geocoder.geocode( {'address': $('#search').val() }, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
 				var lat = results[0].geometry.location.lat();
 				var lng = results[0].geometry.location.lng();
 				var markers = $('#map_canvas').gmap('get', 'markers');
