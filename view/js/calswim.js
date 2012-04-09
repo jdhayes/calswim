4a9e91e578ea28aa42f2f55c33a74e1aed0e9423
Jordan Hayes <hayesjordan@gmail.com>
2012-04-09 10:47:52 -0700

    Changed map to center on OC and zoom in

--- view/js/calswim.js
+++ view/js/calswim.js
@@ -216,10 +216,10 @@ function initialize() {
     // Init Google Map    
     var geocoder = new google.maps.Geocoder();
     // Center initialized map on OC
-    geocoder.geocode( { 'address': 'California'}, function(results, status) {
+    geocoder.geocode( { 'address': 'Orange County, California'}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
           var myOptions = {
-        	  zoom: 4,
+        	  zoom: 10,
 	          center: results[0].geometry.location,
 	          mapTypeId: google.maps.MapTypeId.ROADMAP
           }
