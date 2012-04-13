6af8d4bff580e5061a81705375d0a3eb0d977a59
Jordan Hayes <hayesjordan@gmail.com>
2012-04-13 15:33:30 -0700

    debuggign

--- view/js/calswim.js
+++ view/js/calswim.js
@@ -215,7 +215,7 @@ function initialize() {
     $(".button").button();
     
     function autoResizeGTable(object) {
-    	var this_layout = object.data; //.data("layout");
+    	var this_layout = object.data("layout");
     	alert('DATA: '+this_layout);
     	
     	//for (item in this_layout){    		
