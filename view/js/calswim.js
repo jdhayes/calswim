57823de98ddf549e0679831d1784d076e1bcbe59
Jordan Hayes <hayesjordan@gmail.com>
2012-04-27 17:45:10 -0700

    fixed gtable parser

--- view/js/calswim.js
+++ view/js/calswim.js
@@ -31,7 +31,7 @@ function initTableMap(json_data) {
     // Filter geo data
     var tableGeoView = new google.visualization.DataView(geoData);
     // Filter viewable columns
-    tableGeoView.setColumns([1,2,3,4,5]);
+    tableGeoView.setColumns([1,2,3,4]);
     // Draw Table
     var new_height = $('#table_canvas').height();
     var tableOptions = {width:'100%',height: new_height,page:'disable', showRowNumber:false, cssClassNames:{headerRow:'ui-widget-header'}};    
