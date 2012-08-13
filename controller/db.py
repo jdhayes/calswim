64f1f2ba7582f38dbb968cdd2b83aac87ba9a88d
Jordan Hayes <hayesjordan@gmail.com>
2012-08-13 13:37:50 -0700

    debugging location

--- controller/db.py
+++ controller/db.py
@@ -213,10 +213,11 @@ class WebDB:
                 self.return_message = json.dumps(json_data);
                 return
             for location in locations:
-                if not location:
-                    json_data = {'message':'ERROR:: Empty location.'}
-                    self.return_message = json.dumps(json_data);
-                    return
+                #if not location:
+                json_data = {'message':'ERROR:: Empty location. '+ str(location)}
+                self.return_message = json.dumps(json_data);
+                return
+            
                 # Init reusable list to append location and shapefile
                 locs_shps = []
                 count = count+1
